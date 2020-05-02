f909bd2070f4f56174ad2cb8e595d1ad00be9ab1
dennisou0417 <dennisou0417@users.noreply.github.com>
2020-05-02 01:33:07 -0400

    database and routes

--- Quiz2.0/routes/index.js
+++ Quiz2.0/routes/index.js
@@ -4,16 +4,32 @@ const router = express.Router();
 
 const data = require('../db.json');
 
+router.get('/quiz/list', function(req,res){
+  res.json(data.list);
+})
 
 router.get('/quiz/:quizid', function(req,res){
   var quizid = req.params['quizid'];
 
-  req_id = data["html"].filter(q => q.id == quizid);
-  res.json(req_id);
+  req_quizid = data.list.filter(q => q.quizid == quizid);
+  res.json(req_quizid);
 });
 
 router.get('/quiz/:quizid/:questionid', function(req,res){
-  res.send('Thank you for visiting the page! See you again soon');
+  var quizid = req.params['quizid'];
+  var questionid = req.params['questionid'];
+
+  req_quizid = data.data.filter(q => q.quizid == quizid);
+  req_questionid = data.data.filter(q => q.id == questionid);
+
+  if(quizid == "1" && questionid == "first"){
+    res.json(data.data.filter(q => q.id == "100"));
+  }else if (quizid == "2" && questionid == "first"){
+    res.json(data.data.filter(q => q.id == "200"));
+  }else{
+    res.json(req_questionid);
+  }
+
 });
 
 module.exports = router;
